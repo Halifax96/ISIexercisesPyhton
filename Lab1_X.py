290c067cd90a6a71e613ca4df1eb05cb4b3388e8
Halifax96 <esalap00@gmail.com>
2023-02-24 02:21:16 +0100

    ISIcommit

---
+++ Lab1_X.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 20:33:13 2023
+
+@author: esteb
+"""
+
+#from fractions import Fraction
+
+
+
+print("Exercise 1")
+
+
+
+#val = float(input("Type a Celsius value: "))
+#farenheit =(Fraction(9,5)*val)+32
+
+#print("The vale of Farenheit from Celcius is: %.1f"%(farenheit))
+
+
+print("Exercise 2")
+
+
+#value = int(input("Type the number of data you want to save: "))
+
+#nums = []
+#numspow=[]
+#plusSquare=0
+
+#for i in range(value):
+    
+#   numtyped=float(input())
+   
+#   if not numtyped:
+#       print("There is any number typed")
+   
+#   if numtyped >=0:
+#       nums.append(numtyped)
+#       numspow.append(numtyped**2)
+       
+#   else:
+#       nums.append(numtyped)
+#       numspow.append(numtyped**2)
+#       break
+   
+ 
+#print(nums)
+#print(numspow)
+
+#for plus in numspow:
+#    plusSquare+=plus
+    
+#print(plusSquare)
+    
+    
+    
+    
+print("Exercise 3")
+
+
+#def factorial(n):
+#    if n==1:
+#        return 1
+#    else:
+#        return n*factorial(n-1)
+
+
+#factValue=int(input("Tipe the value of factorial: "))
+
+#if factValue < 0:
+
+#    print("Factorial does not exist for negative numbers")
+
+#elif factValue == 0:
+    
+#    print("The factorial of 0 is 1")
+
+#else:
+    
+#    print("The factorial of", factValue, "is ", factorial(factValue))
+
+print("Exercise 4")
+
+
+
+#valueString = input("")
+#res = " "
+
+#for index in range(len(valueString)):
+#    if index % 2:
+#       res = res + valueString[index].upper()
+#    else:
+#        res = res + valueString[index].lower()
+        
+
+#print(" ",valueString.upper()," ",valueString.lower(), " ", valueString.capitalize(), " ", str(res))
+
+
+print("Exercise 5")
+
+
+#letter=input("Type your spanish ID: ")
+
+#letter=letter.replace(' ', '')
+#letter=letter.replace('.','')
+
+#letter=int(letter)
+
+
+#letterList=['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
+
+#print(letter)
+
+#print("The ID letter is: ", letterList[letter%23])
+
+
+
+
+
+
+
+
+
+
+
